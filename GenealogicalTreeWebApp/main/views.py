19e11b645fccc31b145d33ec69afdc241b802a5a
arif_meilanov <arif.m.04@mail.ru>
2024-10-17 21:35:39 +0300

    transferring data to templates using Jinja

--- GenealogicalTreeWebApp/main/views.py
+++ GenealogicalTreeWebApp/main/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    context = {
+        'title': 'Genealogical Tree Web App',
+        'values': ['val1', 'val2', 'val3'],
+    }
+    return render(request, 'main/index.html', context)
 
 
 def about(request):
